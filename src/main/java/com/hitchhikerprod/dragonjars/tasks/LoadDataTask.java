78e88d8d96e11b115ca5f97ce621929deade53a3
Ben Cordes <cordes.ben@gmail.com>
2025-08-13 16:14:22 -0400

    load executable into a "chunk"

--- src/main/java/com/hitchhikerprod/dragonjars/tasks/LoadDataTask.java
+++ src/main/java/com/hitchhikerprod/dragonjars/tasks/LoadDataTask.java
@@ -13,6 +13,7 @@ public class LoadDataTask extends Task<List<Chunk>> {
     protected List<Chunk> call() throws Exception {
         final List<Chunk> chunks = new ArrayList<>();
         try (
+            final RandomAccessFile exec = new RandomAccessFile("DRAGON.COM", "r");
             final RandomAccessFile data1 = new RandomAccessFile("DATA1", "r");
             final RandomAccessFile data2 = new RandomAccessFile("DATA2", "r")
         ) {
@@ -20,12 +21,20 @@ public class LoadDataTask extends Task<List<Chunk>> {
             final int count = table.getChunkCount();
             for (int i = 0; i < count; i++) {
                 if (isCancelled()) return null;
-                updateMessage("Loading segment " + (i+1) + " of " + count);
+                updateMessage("Loading segment " + (i+1) + " of " + count+1);
                 chunks.add(table.getChunk(i));
-                updateProgress(i+1, count);
+                updateProgress(i+1, count+1);
             }
+            updateMessage("Loading code segment " + count+1 + " of " + count+1);
+            final int codeSize = (int)(exec.length());
+            if ((long)codeSize != exec.length()) {
+                throw new RuntimeException("Executable is too big for one byte array");
+            }
+            final byte[] codeSegment = new byte[codeSize];
+            exec.readFully(codeSegment);
+            chunks.add(new Chunk(codeSegment));
+            updateProgress(count+1, count+1);
             updateMessage("Finished.");
-            updateProgress(count, count);
         }
         return chunks;
     }
