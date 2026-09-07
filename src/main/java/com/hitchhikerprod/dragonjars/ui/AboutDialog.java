654b383d1d67973ea1653a1b082c243002e6ef42
Ben Cordes <cordes.ben@gmail.com>
2026-09-07 12:00:56 -0400

    Dynamically insert app version into properties file at build time

--- src/main/java/com/hitchhikerprod/dragonjars/ui/AboutDialog.java
+++ src/main/java/com/hitchhikerprod/dragonjars/ui/AboutDialog.java
@@ -9,16 +9,18 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.Properties;
 
 public class AboutDialog extends Dialog<Void> {
-    private static final String RESOURCE_NAME = "about.txt";
+    private static final String ABOUT_TEXT = "/com/hitchhikerprod/dragonjars/ui/about.txt";
+    private static final String BUILD_PROPERTIES = "/com/hitchhikerprod/dragonjars/build.properties";
 
     public AboutDialog(Window parent) {
         super();
         super.initOwner(parent);
         super.setResultConverter(buttonType -> null);
         super.setTitle("About DragonJars");
-        super.setHeaderText("DragonJars v0.9-dev\nby Ben Cordes");
+        super.setHeaderText(getHeader());
         super.setContentText(getText());
 
         final ButtonType dismissButton = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
@@ -35,9 +37,26 @@ public class AboutDialog extends Dialog<Void> {
         root.getStylesheets().add(cssUrl.toExternalForm());
     }
 
+    private String getHeader() {
+        final Properties buildProps = new Properties();
+        try (InputStream istream = this.getClass().getResourceAsStream(BUILD_PROPERTIES)) {
+            if (Objects.isNull(istream)) {
+                throw new IOException("Null stream");
+            }
+            buildProps.load(istream);
+        } catch (IOException e) {
+            return "DragonJars\nby Ben Cordes";
+        }
+
+        return "DragonJars v" + buildProps.get("app.version") + "\nby Ben Cordes";
+    }
+
     private String getText() {
-        try (final InputStream textfile = this.getClass().getResourceAsStream(RESOURCE_NAME)) {
-            return new String(Objects.requireNonNull(textfile).readAllBytes(), StandardCharsets.UTF_8);
+        try (final InputStream textFile = this.getClass().getResourceAsStream(ABOUT_TEXT)) {
+            if (Objects.isNull(textFile)) {
+                throw new IOException("Null stream");
+            }
+            return new String(textFile.readAllBytes(), StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
